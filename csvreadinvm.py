139ebb92015a5414221f95922531de382304bfdf
stealthydonut <kinger6688@gmail.com>
2017-05-05 11:34:59 -0400

    Ticker List Gold
    Change the conditions to get the correct ticker list

--- csvreadinvm.py
+++ csvreadinvm.py
@@ -5,6 +5,7 @@ df['firstchar'] = df['phaseone_ticker'].astype(str).str[0]
 #Identify the ones that have a digit in the first spot
 df2=df[df['firstchar'].str.contains('|'.join(searchfor))]
 df2['digit flag']=1
-df3=df2['digit flag','phaseone_ticker']
-
-print df.dtypes
+df3=df2[['digit flag','phaseone_id_bb_company','phaseone_id_bb_parent_co','phaseone_ticker']]
+londonpre=pd.merge(df, df3, how='left',  left_on=['phaseone_id_bb_company','phaseone_id_bb_parent_co','phaseone_ticker'], right_on=['phaseone_id_bb_company','phaseone_id_bb_parent_co','phaseone_ticker'])
+london_ticker=londonpre[londonpre['digit flag']!=1.0]
+print df.dtypespring 
