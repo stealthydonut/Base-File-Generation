19ccadc06c0fa0ff4e4fad410203b604845ca597
stealthydonut <kinger6688@gmail.com>
2017-05-05 11:10:41 -0400

    Identify the first character to eventually exclude

--- csvreadinvm.py
+++ csvreadinvm.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
 df = pd.read_csv('hope/London.csv')
+df['firstchar'] = df['phaseone_ticker'].astype(str).str[0]
+#Identify the ones that have a digit in the first spot
+df2=df[df['firstchar'].str.contains('|'.join(searchfor))]
+df2['digit flag']=1
+df3=df2['digit flag','phaseone_ticker']
+
 print df.dtypes
