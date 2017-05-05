c949cb6d23dad2a83c3cb8f83470e2d24ea44909
stealthydonut <kinger6688@gmail.com>
2017-05-05 12:56:55 -0400

    Update csvreadinvm.py

--- csvreadinvm.py
+++ csvreadinvm.py
@@ -3,6 +3,7 @@ import pandas as pd
 df = pd.read_csv('hope/London.csv')
 df['firstchar'] = df['phaseone_ticker'].astype(str).str[0]
 #Identify the ones that have a digit in the first spot
+searchfor=['1','2','3','4','5','6','7','8','9','0']
 df2=df[df['firstchar'].str.contains('|'.join(searchfor))]
 df2['digit flag']=1
 df3=df2[['digit flag','phaseone_id_bb_company','phaseone_id_bb_parent_co','phaseone_ticker']]
