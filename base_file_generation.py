57cc3f11381b13eb475c638155ae84a007386b4f
stealthydonut <kinger6688@gmail.com>
2017-05-10 10:41:57 -0400

    Add get data from storage

--- base_file_generation.py
+++ base_file_generation.py
@@ -1,3 +1,26 @@
+import pandas as pd
+import numpy as np
+import quandl as qd
+import os
+import time
+import datetime as dt
+import pandas.core.algorithms as algos
+import StringIO
+from google.cloud import storage
+client = storage.Client()
+bucket = client.get_bucket('historyprices')
+# Then do other things...
+blob = bucket.get_blob('lse_history.csv')
+# Define the object
+content = blob.download_as_string()
+#Because the pandas dataframe can only read from buffers or files, we need to take the string and put it into a buffer
+inMemoryFile = StringIO.StringIO()
+inMemoryFile.write(content)
+#When you buffer, the "cursor" is at the end, and when you read it, the starting position is at the end and it will not pick up anything
+inMemoryFile.seek(0)
+#Note - anytime you read from a buffer you need to seek so it starts at the beginning
+#The low memory false exists because there was a lot of data
+bigdata=pd.read_csv(inMemoryFile, low_memory=False)
 #Begin to build methodology to calculate Probability Runs
 #Convert the dataframe to a list so we can use the power of python iteration
 #Positive Runs
