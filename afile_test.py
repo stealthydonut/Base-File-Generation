bea929f7abe4f6177495ddbe56ab01923b4bbec3
stealthydonut <kinger6688@gmail.com>
2017-05-16 12:47:51 -0400

    Update afile_test.py

--- afile_test.py
+++ afile_test.py
@@ -8,9 +8,9 @@ import pandas.core.algorithms as algos
 import StringIO
 from google.cloud import storage
 client = storage.Client()
-bucket = client.get_bucket('basefilegeneration')
+bucket = client.get_bucket('stagingarea')
 # Then do other things...
-blob = bucket.get_blob('lse_history_base.csv')
+blob = bucket.get_blob('lse_history_stagingarea_w_attributes.csv')
 # Define the object
 content = blob.download_as_string()
 #Because the pandas dataframe can only read from buffers or files, we need to take the string and put it into a buffer
@@ -64,9 +64,40 @@ downdayanalysis['six start downday fl'] = np.where(downdayanalysis['six start do
 downdayanalysis['run count']=downdayanalysis['cnt']
 del downdayanalysis['cnt']
 downdayanalysis['cntofruns']=1
- 
+
+#Group the data into a form that can be analyzed
 afile=downdayanalysis[['negative_master_run','first_ma150rank','first1perchangerank','first2perchangerank','last1perchangerank','last2perchangerank','run count','cntofruns','neg1 down day','neg2 down day','neg3 down day','neg4 down day','neg5 down day','neg6 down day',\
 'neg1 down close','neg2 down close','neg3 down close','neg4 down close','neg5 down close','neg6 down close','two start downday fl','three start downday fl']]
- 
 
+#Put the dataset back into storage
+bucket2 = client.get_bucket('analyticalfile')
+df_out = pd.DataFrame(afile)
+df_out.to_csv('afile.csv', index=False)
+blob2 = bucket2.blob('afile.csv')
+blob2.upload_from_filename('afile.csv') 
+
+
+
+
+
+
+downdayanalysisall= afile.groupby(['run count','first_ma150rank'], as_index=False)['cntofruns','neg1 down day','neg2 down day','neg3 down day','neg4 down day','neg5 down day','neg6 down day',\
+'neg1 down close','neg2 down close','neg3 down close','neg4 down close','neg5 down close','neg6 down close'].sum()
+downdayanalysisall['first_ma150rank']=downdayanalysisall['first_ma150rank'].astype(str)
+downdayanalysisall['var']= downdayanalysisall['first_ma150rank']
+ma150 = downdayanalysisall.pivot(index='var', columns='run count', values='cntofruns')
+ma150.columns = ["cnt_" + str(cn).split('.')[0] for cn in ma150.columns]
+ma150.reset_index()
+ma150['index1'] = ma150.index
+ma150['first_ma150rank']=ma150['index1'].str[0:2]
+ma150['tablekey']='ma150'
+ma150['key1']=ma150['first_ma150rank']
+ma150['keynum']=1
+
+#Put the dataset back into storage
+bucket2 = client.get_bucket('analyticalfile')
+df_out = pd.DataFrame(ma150)
+df_out.to_csv('ma150.csv', index=False)
+blob2 = bucket2.blob('ma150.csv')
+blob2.upload_from_filename('ma150.csv') 
   
