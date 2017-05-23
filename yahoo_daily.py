61d69a1180fe93d0441f4140e925c155d41410f6
stealthydonut <kinger6688@gmail.com>
2017-05-23 11:21:06 -0400

    Added all elements to generate file

--- yahoo_daily.py
+++ yahoo_daily.py
@@ -1,10 +1,21 @@
+ticker = ['XOM','MSFT']
+
 import urllib
+import pandas as pd
 
 #Create a loop which iterates through to generate the csv and add the data to a dataframe
 
 #https://stackoverflow.com/questions/2960772/putting-a-variable-inside-a-string-python
 
-link = "http://finance.yahoo.com/d/quotes.csv?s=XOM+BBDb.TO+JNJ+MSFT&f=j1f6oghps7ns"
-f = urllib.urlopen(link)
-myfile = f.read()
-print myfile
+for i in ticker:
+    #Develop the text string that can get all the data
+    start="http://finance.yahoo.com/d/quotes.csv?s="
+    end="&f=j1f6oghps7ns"
+    str1 = ''.join([i])
+    text2=start+str1+end    
+    #Get the data from the yahoo api
+    link=text2
+    f = urllib.urlopen(link)
+    myfile = f.read()
+    df = pd.read_csv(f)
+
